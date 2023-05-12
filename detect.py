87d4dcf6f2b8f1fb816c08ea5fec57608f0c7f9d
kimsy9587 <131329056+kimsy9587@users.noreply.github.com>
2023-05-12 15:52:27 +0900

    Add files via upload

---
+++ detect.py
@@ -0,0 +1,19 @@
+import matplotlib.pyplot as plt
+
+classes = [obj["category_id"] for obj in anno]
+classes = torch.tensor(classes, dtype=torch.int64)
+
+# visualize
+def plot_results(pil_img, prob, boxes):
+    plt.figure(figsize=(16, 10))
+    plt.imshow(pil_img)
+    ax = plt.gca()
+    for p, (xmin, ymin, xmax, ymax), c in zip(prob, boxes.tolist(), COLORS * 100):
+        ax.add_patch(plt.Rectangle((xmin, ymin), xmax - xmin, ymax - ymin,
+                                   fill=False, color=c, linewidth=3))
+        cl = p.argmax()
+        text = f'{classes[cl]}: {p[cl]:0.2f}'
+        ax.text(xmin, ymin, text, fontsize=15,
+                bbox=dict(facecolor='yellow', alpha=0.5))
+    plt.axis('off')
+    plt.show()
